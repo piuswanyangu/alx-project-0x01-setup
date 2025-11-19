f9c35235cd44569955191a2c9da7fdbb2ad98455
pius wanyangu <ndubipius96@gmail.com>
2025-11-19 19:49:18 +0300

    adding layoutheader

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Header: React.FC = ()=>{
+const Header: React.FC = ()=> {
     return(
         <header className="bg-blue-600 text-white shadow-md py-4">
             <div className="container mx-auto flex justify-between items-center px-4">
