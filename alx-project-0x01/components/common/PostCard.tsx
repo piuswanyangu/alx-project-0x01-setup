6da6731f1eb51a472121d589075bdf7c7d668adc
pius wanyangu <ndubipius96@gmail.com>
2025-11-19 19:45:42 +0300

    adding POSTCARDS

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -7,7 +7,7 @@ const PostCard: React.FC<PostProps> = ({ id, title, body, userId }) => {
     <div className="border p-4 rounded">
       <h2 className="text-lg font-semibold">{title}</h2>
       <p className="text-sm">{body}</p>
-      <small>User ID: {userId}</small>
+      <small>Post ID: {id}</small>
     </div>
   );
 };
